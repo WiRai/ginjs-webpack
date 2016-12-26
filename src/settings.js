ec7a5c9a6fd17ab4d93c5e207f54d3bca0e4aed4
raiko <raiko.wielk@u-tg.net>
2016-12-26 21:15:39 +0100

    updated to gin 0.2.x

--- src/settings.js
+++ src/settings.js
@@ -1,17 +1,18 @@
 const path = require('path');
 
-const featurePath = path.join(process.env.PRODUCTLINE_DIR, 'features', 'gap');
+const PRODUCTLINE_DIR = process.env.PRODUCTLINE_DIR;
+const context = require('ginjs').context;
 
 const settings = {
   introduce_webpackConfig: {
-    context: featurePath,
+    context: PRODUCTLINE_DIR,
     devtool: 'source-map',
-    entry: ['./entrypoint'],
+    entry: ['./features/gap/entrypoint'],
     module: {
       loaders: [],
     },
     output: {
-      path: path.join(featurePath, 'dist'),
+      path: path.join(context.DATA_DIR, 'dist'),
       filename: 'bundle.js',
     },
     plugins: [],
